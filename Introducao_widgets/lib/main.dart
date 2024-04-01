2b9a67d49cb2f776edfc8fcd0d088d8a0dc33e95
Fabio313 <fabiozf3108@gmail.com>
2024-04-01 19:16:39 -0300

    correção utilização de projeto correto

--- Introducao_widgets/lib/main.dart
+++ Introducao_widgets/lib/main.dart
@@ -1,64 +1,24 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(MeuMaterialApp());
 
-class MyApp extends StatelessWidget {
+class MeuMaterialApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Cadastro',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: CadastroScreen(),
-    );
-  }
-}
-
-class CadastroScreen extends StatefulWidget {
-  @override
-  _CadastroScreenState createState() => _CadastroScreenState();
-}
-
-class _CadastroScreenState extends State<CadastroScreen> {
-  final TextEditingController _nameController = TextEditingController();
-  final TextEditingController _emailController = TextEditingController();
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Cadastro'),
-      ),
-      body: Padding(
-        padding: EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            TextField(
-              controller: _nameController,
-              decoration: InputDecoration(
-                labelText: 'Nome',
-              ),
-            ),
-            SizedBox(height: 20),
-            TextField(
-              controller: _emailController,
-              decoration: InputDecoration(
-                labelText: 'E-mail',
-              ),
-            ),
-            SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-                // Lógica para processar o cadastro
-                String name = _nameController.text;
-                String email = _emailController.text;
-                // Aqui você pode enviar os dados para onde quiser, como uma API, por exemplo
-                print('Nome: $name, E-mail: $email');
-              },
-              child: Text('Cadastrar'),
-            ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Aplicação test'),
+        ),
+        drawer: const Drawer(),
+        body: const Center(
+          child: Text('Principal'),
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Principal'),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.settings), label: 'Configurações')
           ],
         ),
       ),
