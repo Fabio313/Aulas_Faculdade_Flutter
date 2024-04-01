17ea1487fb30b7fefb4fcd14143eeaa79231cf43
Fabio313 <fabiozf3108@gmail.com>
2024-04-01 19:14:23 -0300

    first form create

--- Introducao_widgets/lib/main.dart
+++ Introducao_widgets/lib/main.dart
@@ -1,24 +1,64 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MeuMaterialApp());
+void main() => runApp(MyApp());
 
-class MeuMaterialApp extends StatelessWidget {
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Aplicação test'),
-        ),
-        drawer: const Drawer(),
-        body: const Center(
-          child: Text('Principal'),
-        ),
-        bottomNavigationBar: BottomNavigationBar(
-          items: const <BottomNavigationBarItem>[
-            BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Principal'),
-            BottomNavigationBarItem(
-                icon: Icon(Icons.settings), label: 'Configurações')
+      title: 'Cadastro',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: CadastroScreen(),
+    );
+  }
+}
+
+class CadastroScreen extends StatefulWidget {
+  @override
+  _CadastroScreenState createState() => _CadastroScreenState();
+}
+
+class _CadastroScreenState extends State<CadastroScreen> {
+  final TextEditingController _nameController = TextEditingController();
+  final TextEditingController _emailController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Cadastro'),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            TextField(
+              controller: _nameController,
+              decoration: InputDecoration(
+                labelText: 'Nome',
+              ),
+            ),
+            SizedBox(height: 20),
+            TextField(
+              controller: _emailController,
+              decoration: InputDecoration(
+                labelText: 'E-mail',
+              ),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                // Lógica para processar o cadastro
+                String name = _nameController.text;
+                String email = _emailController.text;
+                // Aqui você pode enviar os dados para onde quiser, como uma API, por exemplo
+                print('Nome: $name, E-mail: $email');
+              },
+              child: Text('Cadastrar'),
+            ),
           ],
         ),
       ),
